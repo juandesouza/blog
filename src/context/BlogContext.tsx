bd8662801386e7a5f749fde330c15f9aae19f787
Juan de Souza <juandesouza7@gmail.com>
2020-03-05 21:45:52 -0300

    data api sync

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -1,30 +1,32 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 import uuid from 'uuid';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
-        case 'add_blogpost':
-            return [...state, 
-                { 
-                    id: uuid(), 
-                    title: action.payload.title,
-                    content: action.payload.content
-                }
-            ];
         case 'edit_blogpost':
              return state.map(blogPost => {
                 return blogPost.id === action.payload.id ? action.payload : blogPost
-            });            
+            });
+        case 'get_blogposts':
+            return action.payload;
         default:
             return state;
     }
 };
 
+const getBlogPosts = dispatch => {
+    return async () => {
+        const response = await jsonServer.get('/blogposts');
+        dispatch({ type: 'get_blogposts', payload: response.data})
+    };
+};
+
 const addBlogPost = dispatch => {
-    return (title, content, callback) => {
-        dispatch({ type: 'add_blogpost', payload: { title, content} });
+    return async (title, content, callback) => {        
+        await jsonServer.post('/blogposts', { title, content });
         if (callback) {
             callback();
         }
@@ -32,11 +34,15 @@ const addBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-    return id => dispatch({ type: 'delete_blogpost', payload: id });
+    return async id => {
+        await jsonServer.delete(`/blogposts/${id}`)
+        dispatch({ type: 'delete_blogpost', payload: id });
+    }
 }
 
 const editBlogPost = dispatch => {
-    return (id, title, content, callback) => {
+    return async (id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, { title, content });
         dispatch({ type: 'edit_blogpost', payload: { id, title, content}});
         if (callback) {
             callback();
@@ -46,6 +52,6 @@ const editBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    { addBlogPost, deleteBlogPost, editBlogPost },
-    [{ title: 'TEST POST', content: 'TEST CONTENT', id: uuid() }]
+    { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+    []
 );
--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
@@ -8,7 +8,19 @@ interface Props {
 }
 
 const IndexScreen: React.FC<Props> = ({ navigation }) => {
-    const { state, deleteBlogPost } = useContext(Context);
+    const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+
+    useEffect(() => {
+        getBlogPosts();
+
+        const listener = navigation.addListener('didFocus', () => {
+            getBlogPosts();
+        });
+
+        return () => {
+            listener.remove();
+        };
+    }, []);
 
     return (
         <View>
